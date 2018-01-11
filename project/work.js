6d673f3f4b7d7b8236346d5d2399b12bbca05862
leocyx <leo8400725@yahoo.com.tw>
2018-01-11 15:15:15 +0800

    add travis.yml

--- project/work.js
+++ project/work.js
@@ -2,7 +2,6 @@ var M = module.exports = {}
 var eatmoney = new Array()
 var trafficmoney = new Array()
 var leisuremoney = new Array()
-
 var date = new Date()
 var totalm = ''
         function work(option){ 
@@ -61,29 +60,4 @@ var totalm = ''
         M.leisure = function (money){
           totalm = eval(totalm+"+"+money)
           return totalm
-
-var totalm = ''
-        function work(option){
-            var money = document.getElementById("amount")
-            var total = document.getElementById("total")
-            totalm = total.innerText
-            if(option === "eat")total.innerText = M.eat(money.value)
-            else if(option === "traffic")total.innerText = M.traffic(money.value)
-            else if(option === "leisure")total.innerText = M.leisure(money.value)
-        }
-        M.eat = function(money){
-          eatmoney.push(money)
-        totalm = eval(totalm+"+"+money)
-        return  totalm;
-      }
-      M.traffic = function (money){
-          trafficmoney.push(money)
-          totalm = eval(totalm+"+"+money)
-          return  totalm;
-        }
-        M.leisure = function (money){
-          leisuremoney.push(money)
-          totalm = eval(totalm+"+"+money)
-          return  totalm;
-
         }
